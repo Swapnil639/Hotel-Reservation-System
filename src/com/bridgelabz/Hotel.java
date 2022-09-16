24190fc11a23c6310972233eac069a84b7616677
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-16 19:39:30 +0530

    Added :Ability to find the cheapest hotel for the given date range

--- src/com/bridgelabz/Hotel.java
+++ src/com/bridgelabz/Hotel.java
@@ -1,32 +1,88 @@
 package com.bridgelabz;
 
-public class Hotel {
-    private String hotelName;
-    private double weekdayRegularCustomerCost;
-    private double weekendRegularCustomerCost;
-    private double weekdayRewardsCustomerCost;
-    private double weekendRewardsCustomerCost;
 
-    public Hotel() {
+import java.time.LocalDate;
+
+    public class Hotel {
+        private String hotelName;
+        private double weekdayRegularCustomerCost;
+        private double weekendRegularCustomerCost;
+        private double weekdayRewardsCustomerCost;
+        private double weekendRewardsCustomerCost;
+        private double hotelCost;
+
+        public double getHotelCost() {
+            return hotelCost;
+        }
+
+        public void setHotelCost(double hotelCost) {
+            this.hotelCost = hotelCost;
+        }
+
+        public Hotel() {
+        }
+
+        public Hotel(String hotelName, double weekdayRegularCustomerCost, double weekendRegularCustomerCost, double weekdayRewardsCustomerCost, double weekendRewardsCustomerCost) {
+            this.hotelName = hotelName;
+            this.weekdayRegularCustomerCost = weekdayRegularCustomerCost;
+            this.weekendRegularCustomerCost = weekendRegularCustomerCost;
+            this.weekdayRewardsCustomerCost = weekdayRewardsCustomerCost;
+            this.weekendRewardsCustomerCost = weekendRewardsCustomerCost;
+        }
+
+        public String getHotelName() {
+            return hotelName;
+        }
+
+        public void setHotelName(String hotelName) {
+            this.hotelName = hotelName;
+        }
+
+        public double getWeekdayRegularCustomerCost() {
+            return weekdayRegularCustomerCost;
+        }
+
+        public void setWeekdayRegularCustomerCost(double weekdayRegularCustomerCost) {
+            this.weekdayRegularCustomerCost = weekdayRegularCustomerCost;
+        }
+
+        public double getWeekendRegularCustomerCost() {
+            return weekendRegularCustomerCost;
+        }
+
+        public void setWeekendRegularCustomerCost(double weekendRegularCustomerCost) {
+            this.weekendRegularCustomerCost = weekendRegularCustomerCost;
+        }
+
+        public double getWeekdayRewardsCustomerCost() {
+            return weekdayRewardsCustomerCost;
+        }
+
+        public void setWeekdayRewardsCustomerCost(double weekdayRewardsCustomerCost) {
+            this.weekdayRewardsCustomerCost = weekdayRewardsCustomerCost;
+        }
+
+        public double getWeekendRewardsCustomerCost() {
+            return weekendRewardsCustomerCost;
+        }
+
+        public void setWeekendRewardsCustomerCost(double weekendRewardsCustomerCost) {
+            this.weekendRewardsCustomerCost = weekendRewardsCustomerCost;
+        }
+
+        public void getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+
+        }
+
+        @Override
+        public String toString() {
+            return "Hotel{" +
+                    "hotelName='" + hotelName + '\'' +
+                    ", weekdayRegularCustomerCost=" + weekdayRegularCustomerCost +
+                    ", weekendRegularCustomerCost=" + weekendRegularCustomerCost +
+                    ", weekdayRewardsCustomerCost=" + weekdayRewardsCustomerCost +
+                    ", weekendRewardsCustomerCost=" + weekendRewardsCustomerCost +
+                    ", hotelCost=" + hotelCost +
+                    '}';
+        }
     }
-
-    public Hotel(String hotelName, double weekdayRegularCustomerCost, double weekendRegularCustomerCost, double weekdayRewardsCustomerCost, double weekendRewardsCustomerCost) {
-        this.hotelName = hotelName;
-        this.weekdayRegularCustomerCost = weekdayRegularCustomerCost;
-        this.weekendRegularCustomerCost = weekendRegularCustomerCost;
-        this.weekdayRewardsCustomerCost = weekdayRewardsCustomerCost;
-        this.weekendRewardsCustomerCost = weekendRewardsCustomerCost;
-
-    }
-
-    @Override
-    public String toString() {
-        return "Hotel{" +
-                "hotelName='" + hotelName + '\'' +
-                ", weekdayRegularCustomerCost=" + weekdayRegularCustomerCost +
-                ", weekendRegularCustomerCost=" + weekendRegularCustomerCost +
-                ", weekdayRewardsCustomerCost=" + weekdayRewardsCustomerCost +
-                ", weekendRewardsCustomerCost=" + weekendRewardsCustomerCost +
-                '}';
-    }
-}
--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class HotelReservation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation System");
+        System.out.println("Welcome to Hotel Reservation Program");
 
         List<Hotel> hotelDataList = new ArrayList<>();
         Hotel hotel1 = new Hotel("LakeWood",200, 100, 120, 90);
@@ -15,5 +15,14 @@ public class HotelReservation {
         hotelDataList.add(hotel2);
         hotelDataList.add(hotel3);
         hotelDataList.forEach(System.out::println);
+        System.out.println( );
+
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.getDate();
+        hotelReservationSystem.countDays();
+        hotelReservationSystem.findingHotelCost(hotel1);
+        hotelReservationSystem.findingHotelCost(hotel2);
+        hotelReservationSystem.findingHotelCost(hotel3);
+        hotelReservationSystem.comparingHotelAmount(hotel1, hotel2, hotel3);
     }
 }
